2d5041f76ba44016f2a2ce9feab12a5ef63bceaa
Dymas F. Antunes Ribeiro d. Santos <124197980+oDyKoz@users.noreply.github.com>
2026-03-30 10:38:55 -0300

    Enhance main.cpp to include half number calculation
    Added functionality to calculate and display half of the input number.

--- AtividadePratica/atividade1/main.cpp
+++ AtividadePratica/atividade1/main.cpp
@@ -9,7 +9,9 @@
 
 int main(int argc, char** argv) {
 	int numero = 0;
-	
+	float metade; // Define variavel com numero fluante
+
+	// Loop DO WHILE defini e determina se a variavel numero tem numeros negativos ou neutro.
 	do {
 		printf("Digite um numero inteiro positivo: ");
 		scanf("%d", &numero);
@@ -18,11 +20,15 @@ int main(int argc, char** argv) {
 			printf("Numero invalido\n");
 		}
 	} while(numero <= 0);
+
+	// chama a variavel "metade" e armazena o numero dividido pela variavel "numero" inteiro e o numero float, retornando um numero do tipo float. 
+	metade = numero / 2.0;
 	
 	printf("Numero digitado: %d\n", numero);
 	printf("Numero dobrado: %d\n", numero * 2);	
+	printf("Numero metade: %.2f \n", metade); // Delimita o numero float para printar somente com duas casas depois do ponto
 	
-	if(numero / 2) {
+	if((numero % 2) == 0) {
 		printf("Numero par");
 	} else {
 		printf("Numero impar");
